b122cab93c1f352f480dfabc7a6db5d3776ae92b
Matthew Leingang <leingang@nyu.edu>
2023-05-12 08:52:15 -0400

    Initial commit

--- Code.js
+++ Code.js
@@ -1,22 +1,15 @@
 /**
- * Save PDFs emailed from Multifunction Printer to Google Drive
+ * MPL: Can you write a Google App Script to save all PDFs attached
+ * to emails coming from a specific address to a Google Drive folder?
  *
- * ChatGPT, Matthew Leingang
- * 2023-05-09
- *
- * To use this script, follow these steps:
- *
- * 1. Open your Google Drive account.
- * 2. Go to the Google Drive folder where you want to save the PDFs.
- * 3. Click on "New" and select "Google Script".
- * 4. Copy and paste this script into the script editor.
- * 5. Modify the emailAddress, folderName, and printerName variables to suit your specific needs.
- * 6. Click on "Run" and select "savePDFsToDrive".
- * 7. Grant the necessary permissions to the script when prompted.
- * 8. Wait for the script to finish running. The PDFs will be saved to the specified folder in your Google Drive.
- *
+ * ChatGPT: Yes, I can help you write a Google Apps Script to save all 
+ * PDFs attached to emails coming from a specific address to a Google 
+ * Drive folder. Here is an example script that you can modify to suit 
+ * your specific needs:
+ * 
+ * 2023-05-12: Now a github repository
+ * 
  */
- 
 function savePDFsToDrive() {
   // Set the email address to filter
   // TODO: maybe just use a query string?
@@ -40,10 +33,10 @@ function savePDFsToDrive() {
     folder = rootFolder.createFolder(folderName);
   }
 
-  // Get all the threads from the inbox sent by the printer with attachments
+  // Get all the threads in the Inbox
   var threads = GmailApp.search("in:inbox from:" + emailAddress +" has:attachment subject:" + printerName);
 
-  // Loop through all the threads
+  // Loop through all the threads in the Inbox
   for (var i = 0; i < threads.length; i++) {
     console.log("Thread: ", i);
     var thread = threads[i];
@@ -69,10 +62,10 @@ function savePDFsToDrive() {
             // Save the PDF file to the Google Drive folder
             var file = folder.createFile(attachment);
             Logger.log("Saved PDF file: " + file.getName());
-            // Mark thread as read and archive it
             GmailApp.markThreadRead(thread).moveThreadToArchive(thread);
           }
         }
       }
     }
   }
+
